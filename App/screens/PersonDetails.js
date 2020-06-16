3a1c0053e1e7c4dea9067a4d81378e3883cf4c2a
Spencer Carli <spencercarli@gmail.com>
2020-06-16 11:50:57 -0500

    08 Get Data when Using Deep Link

--- App/screens/PersonDetails.js
+++ App/screens/PersonDetails.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, Text, ActivityIndicator, View } from 'react-native';
+import { ScrollView, Text, ActivityIndicator, View, Alert } from 'react-native';
 
 const valueMap = {
   name: 'Name',
@@ -17,12 +17,24 @@ export const PersonDetails = ({ route }) => {
   const { details = {}, id } = params;
 
   const [loading, setLoading] = useState(true);
-
-  console.log('id', id);
+  const [displayDetails, setDisplayDetails] = useState({});
 
   useEffect(() => {
-    if (Object.keys(details).length > 0) {
+    const hasDetails = Object.keys(details).length > 0;
+    if (hasDetails) {
+      setDisplayDetails(details);
       setLoading(false);
+    } else if (id) {
+      fetch(`https://swapi.dev/api/people/${id}`)
+        .then((res) => res.json())
+        .then((res) => {
+          setDisplayDetails(res);
+          setLoading(false);
+        })
+        .catch((error) => {
+          Alert.alert('an error occurred! See console for more info.');
+          console.log(error);
+        });
     }
   }, []);
 
@@ -50,14 +62,14 @@ export const PersonDetails = ({ route }) => {
       }}
     >
       {Object.keys(valueMap).map((key) => {
-        if (!details[key]) {
+        if (!displayDetails[key]) {
           return null;
         }
 
         return (
           <Text style={{ fontSize: 18, marginTop: 10 }} key={key}>
             <Text style={{ fontWeight: 'bold' }}>{`${valueMap[key]}: `}</Text>
-            {details[key]}
+            {displayDetails[key]}
           </Text>
         );
       })}
